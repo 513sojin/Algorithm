ca8f470b4919dcffe258d807442a57ef4c82812b
Sojin Lee <54045865+513sojin@users.noreply.github.com>
2023-08-08 17:31:51 +0900

    [Silver III] Title: N과 M (7), Time: 1616 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+n,m = map(int, input().split())
+num = list(map(int, input().split()))
+ans = []
+
+def dfs():
+    if len(ans) == m:
+        print(' '.join(map(str, ans)))
+        return
+
+    for i in range(len(num)):
+        ans.append(num[i])
+        dfs()
+        ans.pop()
+
+num.sort()
+dfs()
