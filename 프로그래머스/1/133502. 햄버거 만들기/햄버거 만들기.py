263cfb9277076e685ac2b1b38cd8843c2885423d
Sojin Lee <54045865+513sojin@users.noreply.github.com>
2023-10-18 16:23:30 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
@@ -0,0 +1,12 @@
+
+def solution(ingredient):
+    count = 0
+    stack = []
+    
+    for i in ingredient:
+        stack.append(i)
+        if stack[-4:] == [1,2,3,1]:
+            count += 1
+            for _ in range(4):
+                stack.pop()
+    return count 
